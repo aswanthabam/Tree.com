0ef6978381dbca860248785ed907cb4b2de842c1
Aswanth Vc <aswanth.abam@gmail.com>
2024-05-18 01:53:52 +0530

    refactor : minor code refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:tree_com/presentation/pages/home.dart';
 import 'package:tree_com/presentation/pages/register.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: RegisterPage(),
   ));
